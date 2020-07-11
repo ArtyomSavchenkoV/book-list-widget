f90173de58408ffd831d14ad91861dbe67a8cd84
Artyom <a.savchenko@vega-absolute.ru>
2020-07-11 13:39:43 +0700

    To develop a format of the book store.

--- src/reducers/books-store-reducer.ts
+++ src/reducers/books-store-reducer.ts
@@ -4,19 +4,84 @@ import { TAction } from './index';
 *   Commands types
 */
 export type TBooksStoreReducerCommands = {
-    type: null
-} ;
+    type: 'FETCH_BOOKS_REQUESTED'
+} | {
+    type: 'FETCH_BOOKS_SUCCESS',
+    payload: {
+        booksToRead: {
+            id: string,
+            author: string,
+            title: string,
+            description: string,
+            tags: string[]
+        }[],
+        booksInProgress: {
+            id: string,
+            author: string,
+            title: string,
+            description: string,
+            tags: string[]
+        }[],
+        booksDone: {
+            id: string,
+            author: string,
+            title: string,
+            description: string,
+            tags: string[]
+        }[]
+    }
+} | {
+    type: 'FETCH_BOOKS_FAILURE'
+} | {
+    type: 'CHANGE_BOOK_STATUS',
+    payload: {
+        id: string,
+        newStatus: 'TO_READ' | 'IN_PROGRESS' | 'DONE'
+    }
+} | {
+    type: 'SET_FILTER_TAGS',
+    payload: string[]
+};
+
 
 /*
 *   Store type
 */
 export type TBooksStore = {
+    dataStatus: 'EMPTY' | 'LOADING' | 'READY' | 'FAILURE',
+    booksToRead: {
+        id: string,
+        author: string,
+        title: string,
+        description: string,
+        tags: string[]
+    }[],
+    booksInProgress: {
+        id: string,
+        author: string,
+        title: string,
+        description: string,
+        tags: string[]
+    }[],
+    booksDone: {
+        id: string,
+        author: string,
+        title: string,
+        description: string,
+        tags: string[]
+    }[],
+    selectedFilterTags: string[]
 };
 
 /*
 *   Initial store
 */
 const initialBooksStore: TBooksStore = {
+    dataStatus: 'EMPTY',
+    booksToRead: [],
+    booksInProgress: [],
+    booksDone: [],
+    selectedFilterTags: []
 };
 
 /*
